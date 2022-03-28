3388923ff826be7269b1fcbc7168d8a5c6e36c3e
kcs-jinalshah <61497540+kcs-jinalshah@users.noreply.github.com>
2022-03-28 17:10:25 +0530

    'Psotmansave'

---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+var {mongoose}=require('../db/mongoose')
+var Todo=mongoose.model('Todo',{
+    text:{
+        type:String,
+        required:true,
+        minlength:10
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    },
+    completedAt:{
+        type:Number,
+        default:null
+    }
+});
+
+module.exports={
+    Todo
+};
---
+++ server/server.js
@@ -0,0 +1,41 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+var {mongoose}=require('./db/mongoose');
+ var {Todo}=require('./models/todo');
+ var {User}=require('./models/user');
+
+var app=express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo=new Todo({
+        text:req.body.text
+    });
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+    })
+});
+
+app.listen(3000,()=>
+{
+    console.log('Started on port 3000');
+}
+);
+
+
+// var otherTodo=new Todo({
+//     text:'Feed the cat',
+//     completed:true,
+//     completedAt:123
+// });
+
+// otherTodo.save().then((doc)=>{
+//     console.log(JSON.stringify(doc));
+// },(e)=>{
+//     console.log('Unable to save todo');
+// });
