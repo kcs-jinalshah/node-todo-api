3dfeddc182a6df247981343182549d257620a24d
kcs-jinalshah <61497540+kcs-jinalshah@users.noreply.github.com>
2022-03-28 17:37:53 +0530

    'Commit'

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos',(req,res)=>{
     })
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    },(e)=>{
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000,()=>
 {
     console.log('Started on port 3000');
@@ -39,3 +47,7 @@ app.listen(3000,()=>
 // },(e)=>{
 //     console.log('Unable to save todo');
 // });
+
+module.exports={
+    app
+}
